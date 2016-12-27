4affb9138b702a25b8b9a47d8ce87168f4dce8e2
Dvastakana <eugenetaran@ukr.net>
2016-12-27 21:53:22 +0200

    hello commit

--- src/mainpackage/MainSpeedTest.java
+++ src/mainpackage/MainSpeedTest.java
@@ -6,14 +6,7 @@ public class MainSpeedTest {
 
 	public static void main(String[] args){
 		
-		System.out.println("second");
-		System.out.println("third");
-		
-		System.out.println("from new branch newf");
-		
-		System.out.println("new commit in newf");
-		
-		System.out.println("modifyed");
+		System.out.println("hello");
 	}
 	
 	
